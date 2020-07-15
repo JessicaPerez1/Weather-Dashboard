d3423cad0ea2f9178c2e4dc182e69b11443ca0ba
Jessica Perez <1jessicaperez@gmail.com>
2020-07-15 18:03:31 -0400

    cleaned code and comments

--- script.js
+++ script.js
@@ -1,61 +1,42 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-//user types in a city name
-//the cityName is added to a new li
-//then added to display
-//current and 5 day conditions associated with that cityName are displayed to the cards to the left
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
-
-//DEPENDENCIES================================================
-// //SET VARIABLES
+//SET VARIABLES
+var cityUl = $(".search-history");
 var citiesArr = [];
 retrieveInfo();
 onSearchClick();
 
-// API KEY BY CITY NAME
+//API KEY BY CITY NAME ==================
 function onSearchClick() {
   $(".search").on("click", function (event) {
     event.preventDefault();
-    //get the city on button click
+    //get the city's value on button click
     var city = $("#city-name").val();
     //TO MAKE THE first letter capitalized =====
     // var city = cityLower.text(str.charAt(0).toUpperCase() + str.substr(1).toLowerCase());
-    console.log(city);
+    //add the city entered at the end of the citiesArr
     citiesArr.push(city);
-    console.log("CITIEs ARR", citiesArr);
     //SET TO/STORE TO LOCAL STORAGE
     localStorage.setItem("cities", JSON.stringify(citiesArr));
     console.log(localStorage);
     retrieveInfo();
 
-    //TO GET CURRENT DATE==============
+    //GET CURRENT DATE AND CITY - THEN DISPLAY==================
     var date = moment();
     var dateDisplay = date.format("dddd MMMM Do YYYY");
     $(".current-city-date").empty();
     //create a new span for the city
     var newSpanCity = $("<span>");
-    console.log(newSpanCity);
+    //add content
     newSpanCity.text(city);
     newSpanCity.attr("class", "current-city");
     //create a new span for the date
     var newSpanDate = $("<span>");
-    console.log(newSpanDate);
+    //add content
     newSpanDate.text(dateDisplay);
     newSpanDate.attr("class", "current-date");
     //display city and date in card-header
     $(".current-city-date").append(newSpanCity, newSpanDate);
 
-    //API CURRENT WEATHER INFO======================
+    //API CURRENT WEATHER INFO ======================
     var queryURLCurrentTemp =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -65,23 +46,23 @@ function onSearchClick() {
       method: "GET",
     }).then(function (response) {
       console.log(response);
+      //access temp info in api object and display it
       var tempFCurrent = Math.ceil(response.main.temp);
-      console.log(tempFCurrent);
       $("#temperature").text("Temperature: " + tempFCurrent + "F");
+      //access humidity and display it
       var humCurrent = response.main.humidity;
-      console.log(humCurrent);
       $("#humidity").text("Humidity: " + humCurrent + "%");
-      //ACCESS current WIND SPEED
+      //CURRENT WIND SPEED
+      //access wind speed info and display it
       var windSpeedCurrent = response.wind.speed;
-      console.log(windSpeedCurrent);
       $("#wind-speed").text(windSpeedCurrent);
-      //ICON current
+      //CURRENT ICON
+      //access icon info and display it
       var iconCode = response.weather[0].icon;
-      console.log(iconCode);
       var iconImageURL =
         "https://openweathermap.org/img/w/" + iconCode + ".png";
       $("#icon-image").attr("src", iconImageURL);
-      //UV
+      //CURRENT UV INDEX
       var latitude = response.coord.lat;
       var longitude = response.coord.lon;
       var uvURL =
@@ -97,8 +78,7 @@ function onSearchClick() {
         console.log(response);
         var uvIndex = response.value;
         $("#uv-index").text(uvIndex);
-
-        // UV INDEX BACKGROUND COLOR
+        //uv background color change depending on index level
         if (uvIndex < 2) {
           $("#uv-index").attr("class", "low");
           console.log("enjoy the outdoors");
@@ -121,7 +101,7 @@ function onSearchClick() {
         }
       });
 
-      //API 5 DAY FORECAST INFO============
+      //API 5 DAY FORECAST INFO ====================
       var queryURL =
         "https://api.openweathermap.org/data/2.5/forecast?q=" +
         city +
@@ -131,16 +111,14 @@ function onSearchClick() {
         method: "GET",
       }).then(function (response) {
         console.log(response);
-        //ACCESS DATE
-        //day one date
+        //ACCESS DATES FOR 5 FOLLOWING DAYS
+        //day one date access and display
         var dayOneD = moment(response.list[0].dt_txt);
         var dayOneDate = dayOneD.format("dddd MMMM Do YYYY");
-        console.log(dayOneDate);
         $("#day-one-date").text(dayOneDate);
         //day two date
         var dayTwoD = moment(response.list[8].dt_txt);
         var dayTwoDate = dayTwoD.format("dddd MMMM Do YYYY");
-        console.log(dayTwoDate);
         $("#day-two-date").text(dayTwoDate);
         //day three date
         var dayThreeD = moment(response.list[16].dt_txt);
@@ -157,8 +135,8 @@ function onSearchClick() {
         var dayFiveDate = dayFiveD.format("dddd MMMM Do YYYY");
         console.log(dayFiveDate);
         $("#day-five-date").text(dayFiveDate);
-        //ACCESS THE HUMIDITY
-        //humidity day one
+        //ACCESS THE HUMIDITY FOR 5 FOLLOWING DAYS
+        //humidity day one access and display
         var humidityDayOne = response.list[0].main.humidity;
         console.log(humidityDayOne);
         $("#hum-day-one").text("Humidity: " + humidityDayOne + "%");
@@ -178,67 +156,58 @@ function onSearchClick() {
         var humidityDayFive = response.list[32].main.humidity;
         console.log(humidityDayFive);
         $("#hum-day-five").text("Humidity: " + humidityDayFive + "%");
-        //ICON day one
-        console.log(response);
+        //ACCESS ICON FOR 5 FOLLOWING DAYS
+        //icon day one access and display
         var iconCodeDayOne = response.list[0].weather[0].icon;
         var iconImageURLDayOne =
           "https://openweathermap.org/img/w/" + iconCodeDayOne + ".png";
         console.log(iconCodeDayOne);
         $("#icon-day-one").attr("src", iconImageURLDayOne);
-        //ICON day two
+        //icon day two
         var iconCodeDayTwo = response.list[8].weather[0].icon;
         var iconImageURLDayTwo =
           "https://openweathermap.org/img/w/" + iconCodeDayTwo + ".png";
-        console.log(iconCodeDayTwo);
         $("#icon-day-two").attr("src", iconImageURLDayTwo);
-        //ICON day three
+        //icon day three
         var iconCodeDayThree = response.list[16].weather[0].icon;
         var iconImageURLDayThree =
           "https://openweathermap.org/img/w/" + iconCodeDayThree + ".png";
-        console.log(iconCodeDayThree);
         $("#icon-day-three").attr("src", iconImageURLDayThree);
-        //ICON day four
+        //icon day four
         var iconCodeDayFour = response.list[24].weather[0].icon;
         var iconImageURLDayFour =
           "https://openweathermap.org/img/w/" + iconCodeDayFour + ".png";
-        console.log(iconCodeDayFour);
         $("#icon-day-four").attr("src", iconImageURLDayFour);
-        //ICON day five
+        //icon day five
         var iconCodeDayFive = response.list[32].weather[0].icon;
         var iconImageURLDayFive =
           "https://openweathermap.org/img/w/" + iconCodeDayFive + ".png";
         console.log(iconCodeDayFive);
         $("#icon-day-five").attr("src", iconImageURLDayFive);
-        //WEATHER IN PROPER FARHENHEIT UNIT
+        //API WEATHER 5 DAY FORECAST IN PROPER FARHENHEIT UNIT ==================
         var queryURLTemp =
           "https://api.openweathermap.org/data/2.5/forecast?q=" +
           city +
           "&appid=6930be6df36a7c0ee230985f8b10373e&units=imperial";
-        //ACCESS THE 5 day weather temp - display IN CARD1
         $.ajax({
           url: queryURLTemp,
           method: "GET",
         }).then(function (response) {
           console.log(response);
-          //day one temp
+          //day one temp access and display
           var tempFDayOne = Math.ceil(response.list[0].main.temp);
-          console.log(tempFDayOne);
           $("#temp-day-one").text("Temperature: " + tempFDayOne + "F");
           //day two temp
           var tempFDayTwo = Math.ceil(response.list[8].main.temp);
-          console.log(tempFDayTwo);
           $("#temp-day-two").text("Temperature: " + tempFDayTwo + "F");
           //day three temp
           var tempFDayThree = Math.ceil(response.list[16].main.temp);
-          console.log(tempFDayThree);
           $("#temp-day-three").text("Temperature: " + tempFDayThree + "F");
           //day four temp
           var tempFDayFour = Math.ceil(response.list[24].main.temp);
-          console.log(tempFDayFour);
           $("#temp-day-four").text("Temperature: " + tempFDayFour + "F");
           //day five temp
           var tempFDayive = Math.ceil(response.list[32].main.temp);
-          console.log(tempFDayive);
           $("#temp-day-five").text("Temperature: " + tempFDayive + "F");
         });
       });
@@ -246,21 +215,36 @@ function onSearchClick() {
   });
 }
 
-// //LOCAL STORAGE
-//RETRIEVE LOCAL STORAGE INFO
+// LOCAL STORAGE =========================
+//retrieve local storage info
 function retrieveInfo() {
   citiesArr = JSON.parse(localStorage.getItem("cities")) || [];
-  console.log(" NCA", citiesArr);
   $(".search-history").empty();
   //to make sure there are no duplicates stored
-  // //var noDuplicatesArr = [...new Set(citiesArr)];
-  //for all elements of the citiesArr stored, create a p tag and append to the search history list
+  //var noDuplicatesArr = [...new Set(citiesArr)];
+  //display retrieved info to my page - loop through citiesArr
   for (var i = 0; i < citiesArr.length; i++) {
     console.log(citiesArr[i]);
     var pTag = $("<p>");
     pTag.text(citiesArr[i]);
+    //last city searched appears first
     $(".search-history").prepend(pTag);
-    //Add an on click event when one of the ptags are clicked
-    //if one of the .search-history children gets clicked, then run the onSearchClick() function
   }
+  // onUlClick();
 }
+// //WHEN AN ELEMENT INSIDE OF THE CITY SEARCH HISTORY IS CLICKED === TODO ===
+// function onUlClick() {
+//   cityUl.on("click", function (event) {
+//     //prevent bubbling
+//     event.preventDefault();
+//     var element = event.target;
+//     // var element = event.target;
+//     //if that element is a ptag
+//     var pTag = $("<p>");
+//     var findUl = $(".search-history").find(pTag);
+//     if (element === pTag) {
+//       onSearchClick();
+//     }
+//   });
+// }
+// onUlClick();
